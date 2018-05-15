ca6a86e962ff92b4acf62ad294a0d052519b0737
Brian Torluemke <briantorluemke@gmail.com>
2018-05-15 15:38:21 -0500

    adeed switch statement

--- src/FileIO.java
+++ src/FileIO.java
@@ -9,15 +9,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileIO {
+    private static String directory = "data";
+    private static String filename = "contacts.txt";
+    private static Input input = new Input();
+    private static boolean exitApp = false;
 
     public static void main(String[] args) {
-        String directory = "data";
-        String filename = "contacts.txt";
-        Input input = new Input();
-        input.yesNo();
         System.out.println(makeList());
-
         createFileIfNotExists(directory, filename);
+
+        do {
+            selection(menu());
+        } while (!exitApp);
+
+    }
+
+    private static int menu() {
+        System.out.print(
+                "1. View contacts.\n" +
+                        "2. Add a new contact.\n" +
+                        "3. Search a contact by name.\n" +
+                        "4. Delete an existing contact.\n" +
+                        "5. Exit.\n");
+
+        return input.getInt(1, 5);
+
+    }
+
+    private static void selection(int selected) {
+
+        switch (selected) {
+            case 1:
+                System.out.println("You choose view.");
+//                view(contacts.list());
+                break;
+            case 2:
+                System.out.println("You choose add.");
+//                add();
+                break;
+            case 3:
+                System.out.println("You choose search.");
+//                search();
+                break;
+            case 4:
+                System.out.println("You choose delete.");
+//                delete();
+                break;
+            case 5:
+                System.out.println("You choose to leave me.");
+//                exit();
+                break;
+
+        }
+
+    }
+
 
 //        ArrayList<String> items = makeList();
 //        System.out.println(items);
@@ -27,15 +73,15 @@ public class FileIO {
 //        } catch(IOException e) {
 //            System.out.println(e.getMessage());
 //        }
-        try {
-            readLines(directory, filename);
-        } catch(IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-
-    }
-
+//        try {
+//            readLines(directory, filename);
+//        } catch(IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//
+//    }
+//
     public static void createFileIfNotExists(String directory, String filename) {
 
         Path dataDirectory = Paths.get(directory);
